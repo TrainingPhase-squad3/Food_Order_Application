fec6a3a1bcb95639727ab09b9f6ac486eb35dc9b
CHAITRA P SHETTY <64059838+Chaitrashetty2001@users.noreply.github.com>
2023-03-02 10:05:17 +0530

    Update FoodVendorService.java

--- src/main/java/com/squad3/service/FoodVendorService.java
+++ src/main/java/com/squad3/service/FoodVendorService.java
@@ -1,14 +1,14 @@
 
 package com.squad3.service;
 
-<<<<<<< HEAD
+
 import com.squad3.response.ApiResponse;
-=======
+
 import java.util.List;
 import java.util.Map;
 
 import com.squad3.response.SearchResponse;
->>>>>>> origin/manoj
+
 
 public interface FoodVendorService {
 	Map<String, List<SearchResponse>> search(String key);
