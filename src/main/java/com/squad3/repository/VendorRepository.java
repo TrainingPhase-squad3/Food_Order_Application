e9aae172d78c3a6cf27c4c839a079885dd1539ec
Darshan S N <95928626+darshansn19@users.noreply.github.com>
2023-03-02 10:10:10 +0530

    Update VendorRepository.java

--- src/main/java/com/squad3/repository/VendorRepository.java
+++ src/main/java/com/squad3/repository/VendorRepository.java
@@ -7,10 +7,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.squad3.entity.Vendor;
 
 public interface VendorRepository extends JpaRepository<Vendor, Long> {
-<<<<<<< HEAD
+
 	List<Vendor> findByVendorNameContainingIgnoreCase(String key);
 
-=======
+
 	List<Vendor> findAllByVendorNameIgnoreCaseIn(List<String> vendorNames);
->>>>>>> 513aa71789a872ac72fa7aed94c6df0af36ddeea
+
 }
