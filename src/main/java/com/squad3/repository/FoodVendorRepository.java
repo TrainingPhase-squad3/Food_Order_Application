a4df533ed09cc1e5cc5c83fc0c8cc238b328fad9
vamsha591 <65599757+vamsha591@users.noreply.github.com>
2023-03-02 10:09:15 +0530

    Update FoodVendorRepository.java

--- src/main/java/com/squad3/repository/FoodVendorRepository.java
+++ src/main/java/com/squad3/repository/FoodVendorRepository.java
@@ -3,10 +3,7 @@ package com.squad3.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
-=======
-import java.util.List;
->>>>>>> 513aa71789a872ac72fa7aed94c6df0af36ddeea
+
 
 import com.squad3.entity.FoodVendor;
 import com.squad3.entity.Vendor;
@@ -16,9 +13,7 @@ public interface FoodVendorRepository extends JpaRepository<FoodVendor, Long> {
 
 	List<FoodVendor> findByVendorIn(List<Vendor> vendorList);
 
-<<<<<<< HEAD
-=======
-public interface FoodVendorRepository extends JpaRepository<FoodVendor, Long>{
+
 	List<FoodVendor> findAllByVendorVendorNameIgnoreCaseInAndFoodNameIgnoreCaseIn(List<String> vendorNames, List<String> foodNames);
->>>>>>> 513aa71789a872ac72fa7aed94c6df0af36ddeea
+
 }
