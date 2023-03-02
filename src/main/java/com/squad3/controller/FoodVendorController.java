b72ef7aa0026e32b1abf9d46f99482d1eee17314
darshansn19 <darshusn19@gmail.com>
2023-03-02 09:33:02 +0530

    add-foodvendor updated

--- src/main/java/com/squad3/controller/FoodVendorController.java
+++ src/main/java/com/squad3/controller/FoodVendorController.java
@@ -1,12 +1,26 @@
 package com.squad3.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.squad3.response.ApiResponse;
+import com.squad3.serviceimpl.FoodVendorServiceImpl;
 
 @RestController
 @RequestMapping(value = "/food-vendors")
 public class FoodVendorController {
 
+	@Autowired
+	FoodVendorServiceImpl foodVendorServiceImpl;
+	
+	@PostMapping
+	public ResponseEntity<ApiResponse> addFoodVendors() {
+		return ResponseEntity.status(HttpStatus.CREATED).body(foodVendorServiceImpl.addFoodVendors());
+	}
 	
 	
 }
--- src/main/java/com/squad3/service/FoodVendorService.java
+++ src/main/java/com/squad3/service/FoodVendorService.java
@@ -1,6 +1,9 @@
 
 package com.squad3.service;
 
+import com.squad3.response.ApiResponse;
+
 public interface FoodVendorService {
 
+	ApiResponse addFoodVendors();
 }
