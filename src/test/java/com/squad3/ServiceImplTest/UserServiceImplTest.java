691929a632c5921b3fe6852c4e02d0740fafb8d3
CHAITRA P SHETTY <64059838+Chaitrashetty2001@users.noreply.github.com>
2023-03-03 10:46:05 +0530

    Update UserServiceImplTest.java

--- src/test/java/com/squad3/ServiceImplTest/UserServiceImplTest.java
+++ src/test/java/com/squad3/ServiceImplTest/UserServiceImplTest.java
@@ -43,34 +43,68 @@ class UserServiceImplTest {
 	
 	
 	@Test
-	public void testGetOrderHistoryReturnsEmptyListForInvalidTimeframe() {
-	    
-	    Long userId = 123L;
-	    String timeframe = "invalid";
-	    Mockito.when(userRepository.findById(userId)).thenReturn(Optional.of(new User(userId, "chaitra", "c@gmail.com")));
-	   
-	    List<OrderHistoryResponse> result = userService.getOrderHistory(userId, timeframe);
-	   
-	    assertNotNull(result);
-	    assertTrue(result.isEmpty());
+	public void testGetOrderHistory_InvalidUserId() {
+		userService.getOrderHistory(-1L, "week");
+	}
+
+	@Test
+	public void getOrderHistoryTest() {
+		Long userId = 1L;
+		String timeframe = "week";
+		List<Orders> ordersList = new ArrayList<>();
+		Orders order1 = new Orders();
+		order1.setOrderId(1L);
+		order1.setOrderDate(LocalDate.now());
+		order1.setTotalPrice(2);
+		List<OrderItem> orderItems1 = new ArrayList<>();
+		orderItems1.add(new OrderItem(1L, "food1", "vendor1", 2));
+		orderItems1.add(new OrderItem(2L, "food2", "vendor2", 3));
+		order1.setOrderItem(orderItems1);
+		ordersList.add(order1);
+		Mockito.when(userRepository.findById(userId)).thenReturn(Optional.of(new User()));
+		Mockito.when(ordersRepository.findByUser_UserIdAndOrderDateBetween(anyLong(), any(LocalDate.class),
+				any(LocalDate.class))).thenReturn(ordersList);
+
+		List<OrderHistoryResponse> orderHistory = userServiceImpl.getOrderHistory(userId, timeframe);
+
+		assertNotNull(orderHistory);
+		assertEquals(1, orderHistory.size());
+		assertEquals(order1.getOrderDate(), orderHistory.get(0).getOrderDate());
+		assertEquals(order1.getTotalPrice(), orderHistory.get(0).getTotalPrice());
+		assertEquals(order1.getOrderItem().size(), orderHistory.get(0).getOrderIem().size());
+	}
+
+	@Test
+	public void getOrderHistoryInvalidTimeframeTest() {
+		Long userId = 1L;
+		String timeframe = "invalid_timeframe";
+		Mockito.when(userRepository.findById(userId)).thenReturn(Optional.of(new User()));
+
+		userService.getOrderHistory(userId, timeframe);
+	}
+
+	@Test
+	public void getOrderHistoryNoOrderHistoryFoundTestForWeek() {
+		Long userId = 1L;
+		String timeframe = "week";
+		Mockito.when(userRepository.findById(userId)).thenReturn(Optional.of(new User()));
+		Mockito.when(ordersRepository.findByUser_UserIdAndOrderDateBetween(anyLong(), any(LocalDate.class),
+				any(LocalDate.class))).thenReturn(Collections.emptyList());
+
+		userService.getOrderHistory(userId, timeframe);
 	}
 	
-	public void testGetOrderHistoryForValidInput() {
-		 
-		   Long userId = 1L;
-		   String timeframe = "week";
-		 
-		   List<OrderHistoryResponse> orderHistoryResponses =userService.getOrderHistory(userId, timeframe);
+	@Test
+	public void getOrderHistoryNoOrderHistoryTestForMonth() {
+		Long userId = 1L;
+		String timeframe = "month";
+		Mockito.when(userRepository.findById(userId)).thenReturn(Optional.of(new User()));
+		Mockito.when(ordersRepository.findByUser_UserIdAndOrderDateBetween(anyLong(), any(LocalDate.class),
+				any(LocalDate.class))).thenReturn(Collections.emptyList());
 
-		   assertNotNull(orderHistoryResponses);
-		   assertFalse(orderHistoryResponses.isEmpty());
-		}
-	
+		userService.getOrderHistory(userId, timeframe);
+	}
 
-
-
-
-	
 	
 
 }
